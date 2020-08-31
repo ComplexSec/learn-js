864836ccba0aecc6d6e601b09d8812eb7284ce87
ComplexSec <JPAKE200@caledonian.ac.uk>
2020-08-31 23:58:45 +0100

    Create dog_years.js

---
+++ Dog Years Project/dog_years.js
@@ -0,0 +1,15 @@
+// Dog Years Project
+// Jonathan Pake	31-08-2020
+
+const myAge = 24; // Variable initialization
+let earlyYears = 2; // Variable initialization
+
+earlyYears *= 10.5; // Multiply & store
+
+let laterYears = myAge - 2; // Variable initialization
+laterYears *= 4; // Multiply by 4 & store
+
+myAgeInDogYears = earlyYears + laterYears; // Adding & storing
+myName = 'JJ'.toLowerCase(); // convert to lowercase
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} in dog years`);
